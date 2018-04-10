e080ec1909b20d3435ae26567cc2ae1f282dea42
larry <ladams1776@yahoo.com>
2018-04-10 06:21:39 -0400

    created out Student Entity and annotated it to correspond with the student table.

---
+++ src/com/luv2code/jdbc/Student.java
@@ -0,0 +1,25 @@
+package com.luv2code.jdbc;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student") // actually optional, because the class name has the same name as the database table
+public class Student {
+
+	
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+}
